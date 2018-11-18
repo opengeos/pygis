ef93da3c178d39191fc6bbaae142bf7c8cfe8bb5
Qiusheng Wu <giswqs@gmail.com>
2018-11-17 23:43:16 -0500

    Bump version: 0.1.2 → 0.1.3

--- pygis/__init__.py
+++ pygis/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Qiusheng Wu"""
 __email__ = 'giswqs@gmail.com'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 from .zipfile import read_zipfile, write_zipfile, extract_zipfile, get_all_file_paths
 from .download import download_from_gdrive, download_from_url
