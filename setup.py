9d2ed86b78b08b4a7ad52415c699cc3bc4f9b627
Qiusheng Wu <giswqs@gmail.com>
2022-06-06 10:18:32 -0400

    Bump version: 0.1.5 → 0.2.0

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/giswqs/pygis',
-    version='0.1.5',
+    version='0.2.0',
     zip_safe=False,
 )
