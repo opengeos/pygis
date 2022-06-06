2117272a5c23c0e141a407c213034d8e23729a05
Qiusheng Wu <giswqs@gmail.com>
2022-06-06 09:49:36 -0400

    Removed GDAL

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ here = op.abspath(op.dirname(__file__))
 
 # get the dependencies and installs
 with io.open(op.join(here, "requirements.txt"), encoding="utf-8") as f:
-    all_reqs = f.read().split("\n")[1:]
+    all_reqs = f.read().split("\n")
 
 install_requires = [x.strip() for x in all_reqs if "git+" not in x]
 dependency_links = [x.strip().replace("git+", "") for x in all_reqs if "git+" not in x]
