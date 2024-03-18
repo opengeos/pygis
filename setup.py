82344cd3e12d3eeffa14dc59ec249bf5d0ac16aa
Qiusheng Wu <giswqs@gmail.com>
2024-03-18 09:48:55 -0400

    Bump version: 0.6.1 → 0.6.2

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/giswqs/pygis",
-    version="0.6.1",
+    version="0.6.2",
     zip_safe=False,
 )
